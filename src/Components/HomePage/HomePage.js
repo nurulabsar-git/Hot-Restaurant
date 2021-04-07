346a2e358faad62504f0c954f4ad29c166ed24b0
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-04-07 06:46:41 +0600

    update

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import SubHomePage from '../SubHomePage/SubHomePage';
-
+import './HomePage.css'
 const HomePage = () => {
 const [products, setProducts] = useState([])
 
@@ -13,7 +13,7 @@ useEffect(()=> {
 
 
     return (
-        <div>
+        <div className="container">
             <h1>This is home page</h1>
            {
                products.map(pd => <SubHomePage productAttribute = {pd}></SubHomePage>)
--- src/Components/SubHomePage/SubHomePage.js
+++ src/Components/SubHomePage/SubHomePage.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './SubHomePage.css';
 
 const SubHomePage = (props) => {
+    console.log(props)
+    const {_id, imageURL, name} = props.productAttribute;
     return (
-        <div>
-           <h4>THIS IS SUB HOME PAGE</h4> 
+        <div className="sub-home-page">
+          <img src={imageURL} style={{width: '50%'}} alt=""/>
+          <h4>{name?.name}</h4>
+            <p>${name?.price}</p>
+            <p>{name?.wight} gm</p>
+            <button className="main-button">Buy Now</button>
         </div>
     );
 };
