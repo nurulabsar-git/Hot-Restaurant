5e27a9feb6a1641b0605ce49c2f2856adc529b5e
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-04-07 06:15:43 +0600

    update

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,12 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import SubHomePage from '../SubHomePage/SubHomePage';
 
 const HomePage = () => {
-const [products, setProducts] = useState()
+const [products, setProducts] = useState([])
+
+useEffect(()=> {
+  fetch('http://localhost:9000/getProduct')
+  .then(result => result.json())
+  .then(data => setProducts(data))
+
+}, [])
 
 
     return (
         <div>
             <h1>This is home page</h1>
+           {
+               products.map(pd => <SubHomePage productAttribute = {pd}></SubHomePage>)
+           }
         </div>
     );
 };
---
+++ src/Components/SubHomePage/SubHomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SubHomePage = (props) => {
+    return (
+        <div>
+           <h4>THIS IS SUB HOME PAGE</h4> 
+        </div>
+    );
+};
+
+export default SubHomePage;
